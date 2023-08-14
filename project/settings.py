89f54c5c2687987bd168081dc1f342baabdfe7b1
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-08-14 19:50:46 +0300

    tickets app

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'tickets',
 ]
+
+
+REST_FRAMEWORK = {}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
